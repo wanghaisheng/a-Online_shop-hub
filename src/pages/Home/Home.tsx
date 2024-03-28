a4e0b3100914845dfd5ec84313f6345181b306c4
RosinskaK <rosinska.eu@gmail.com>
2024-03-28 18:39:40 +0100

    adding Tailwind

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,14 @@
 // import React from 'react';
-import styles from './home.module.css';
+import styles from "./home.module.css";
 
 function Home() {
   return (
     <div>
       <div className={styles.hello}>Hello Online_shop!</div>
-      <p className={styles.p}>bcuwbciwubec wuvbcwuibvuie jubeioebvoieb</p>
+      <p className="mx-5 p-4 text-xl font-extrabold text-pink-400">
+        bcuwbciwubec wuvbcwuibvuie jubeioebvoieb aerharehberhb aegwgh aerge
+        aerh4h
+      </p>
     </div>
   );
 }
