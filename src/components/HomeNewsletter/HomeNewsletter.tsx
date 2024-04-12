4391d8a7a2b866435a875e41d017f757b29e4953
RosinskaK <rosinska.eu@gmail.com>
2024-04-12 21:28:12 +0200

    HomeNewsletter component rwd

--- src/components/HomeNewsletter/HomeNewsletter.tsx
+++ src/components/HomeNewsletter/HomeNewsletter.tsx
@@ -1,38 +1,39 @@
-//import React from 'react'
 import styles from "../HomeNewsletter/homeNewsletter.module.css";
 import { LiaArrowRightSolid } from "react-icons/lia";
 
 
 function HomeNewsletter() {
   return (
-    <section className=" flex h-72 w-screen bg-black text-white ">
-      <div className=" h-72 w-full p-20 pr-10">
-        <h2 className=" text-4xl leading-8">
+    <section className="flex flex-col w-full bg-black text-white p-5 lg:flex-row h-full">
+
+      <div className="p-6 md:p-20 lg:pr-2 w-full">
+        <h2 className="text-3xl sm:text-4xl leading-7 sm:leading-8 font-semibold">
           STAY UP-TO-DATE ON THE LATEST TRENDS AND EXCLUSIVE OFFERS
         </h2>
-        <p className="pt-3 text-xl font-extralight">
+        <p className="sm:pt-3 sm:text-xl font-extralight text-lg pt-2">
           SUBSCRIBE TO OUR NEWSLETTER
         </p>
       </div>
-      <div className="h-72 w-full  p-20  pl-10">
-        <form className="flex  h-full  flex-col ">
+
+      <div className="h-full w-full p-6 md:p-20 pt-5 md:pt-0 lg:pt-20">
+        <form className="flex flex-col">
           <span className="flex">
             <input
-              className=" w-[400px] rounded-none border-[1px]  bg-transparent p-1 text-lg focus-visible:outline-none"
+              className="w-[220px] sm:w-[400px] rounded-none border-[1px] bg-transparent p-1 sm:text-lg focus-visible:outline-none text-sm"
               type="email"
               id="email"
               placeholder="ENTER YOUR E-MAIL ADDRESS"
             ></input>
-            <button className="block h-full w-[40px] cursor-pointer border-[1px] border-l-0">
+            <button className="block sm:h-[37.6px] h-full sm:w-[40px] cursor-pointer border-[1px] border-l-0 w-[32px]">
               {" "}
-              <LiaArrowRightSolid size={30} className="pl-1" />
+              <LiaArrowRightSolid size={30} className="pl-1 pr-1 sm:pr-0" />
             </button>
           </span>
-          <div className="mt-5  flex items-start">
+          <div className="mt-5 flex items-start">
             <label className={styles.form_checkbox}>
               <input type="checkbox" />
               <span className={styles.checkbox}></span>
-              <p className="max-w-[450px] font-extralight">
+              <p className=" font-extralight pr-14 2xl:max-w-[750px]">
                 I have read and understand the information regarding the use of
                 my personal data contained in the Privacy Policy and consent to
                 receiving personalized commercial communications from
